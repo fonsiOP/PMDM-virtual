11777dcc9c0203f7e624833990b47a1ac83ef2a4
fonsiOP <152197044+fonsiOP@users.noreply.github.com>
2025-09-29 20:17:02 +0200

    up

--- Introduccion/src/app/app.ts
+++ Introduccion/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Semaforo } from './components/semaforo/semaforo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Semaforo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ Introduccion/src/app/components/semaforo/semaforo.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-semaforo',
+  imports: [],
+  templateUrl: './semaforo.html',
+  styleUrl: './semaforo.css'
+})
+export class Semaforo {
+
+
+
+
+}
