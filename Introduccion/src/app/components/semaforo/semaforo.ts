1ffb5fa73c6ad9d1eb7921a9ae3dbd4a98da6594
fonsiOP <152197044+fonsiOP@users.noreply.github.com>
2025-10-06 20:38:39 +0200

    up

--- Introduccion/src/app/components/semaforo/semaforo.ts
+++ Introduccion/src/app/components/semaforo/semaforo.ts
@@ -8,13 +8,32 @@ import { Component } from '@angular/core';
 })
 export class Semaforo {
 
-  numero:number=0;
+  numero:number=10;
+  frutas:string[]=["pera", "banana", "manzana", "uva"];
 
+  casillas:number[]=[0,0,0,0,0,1,0,0,0];
 
 
   incrementar(){
-    this.numero++;
+    if(this.numero<10){
+        this.numero++;
+    }else{
+      this.numero=10;
+    }
   }
+
+  decrementar(){
+    if(this.numero>0){
+        this.numero--;
+    }else{
+      this.numero=0;
+    }
+  }
+
+  resetear(){
+    this.numero=10;
+  }
+
 
 
 
