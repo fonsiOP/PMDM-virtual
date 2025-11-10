cf21ed5f1214cebfc770791c0a032a287a046c4f
fonsiOP <152197044+fonsiOP@users.noreply.github.com>
2025-11-10 20:17:15 +0100

    up

--- Introduccion/src/app/components/lista-personajes/lista-personajes.ts
+++ Introduccion/src/app/components/lista-personajes/lista-personajes.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../../models/personaje';
+import { TarjetaPersonaje } from "../tarjeta-personaje/tarjeta-personaje";
 
 @Component({
   selector: 'app-lista-personajes',
-  imports: [],
+  imports: [TarjetaPersonaje],
   templateUrl: './lista-personajes.html',
   styleUrl: './lista-personajes.css'
 })
 export class ListaPersonajes {
   personajes: Personaje[] = [ 
-    { nombre: 'juan', clase: 'guerrero', nivel: 25, imagen:'ruta de la imagen' },
-    { nombre: 'Jalinda', clase: 'arquero', nivel: 30, imagen:'ruta de la imagen' },
-    { nombre: 'Jenifer', clase: 'mago', nivel: 50, imagen:'ruta de la imagen' },
-    { nombre: 'Gerald', clase: 'brujo', nivel: 51, imagen:'ruta de la imagen' },  
+    { nombre: 'juan', clase: 'guerrero', nivel: 25, imagen:'/images/brujo.jpg' },
+    { nombre: 'Jalinda', clase: 'arquero', nivel: 30, imagen:'/images/jennefer.png' },
+    { nombre: 'Jenifer', clase: 'mago', nivel: 50, imagen:'/images/jennefer.png' },
+    { nombre: 'Gerald', clase: 'brujo', nivel: 51, imagen:'/images/brujo.jpg' },  
   
   ];
 
--- Introduccion/src/app/components/tarjeta-personaje/tarjeta-personaje.ts
+++ Introduccion/src/app/components/tarjeta-personaje/tarjeta-personaje.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../../models/personaje';
 
 @Component({
   selector: 'app-tarjeta-personaje',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './tarjeta-personaje.css'
 })
 export class TarjetaPersonaje {
+
+  @Input() personajeEnviado!:Personaje;
 
 }
